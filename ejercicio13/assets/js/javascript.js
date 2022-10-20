a3b10889cb0dec2950c9ebb6330d1261dc9c6752
ubyquit <ingsystem.out.print@gmail.com>
2022-10-20 11:08:52 -0500

    Operaciones logicos

--- ejercicio13/assets/js/javascript.js
+++ ejercicio13/assets/js/javascript.js
@@ -1,24 +1,14 @@
 x = prompt("Ingresa el valor de x");
 y = prompt("Ingresa el valor de y");
 
-document.write("x: " + x);
-document.write("<br>");
-document.write("y: " + y);
-document.write("<br>");
-document.write("<br>");
-document.write("Igual: " + (x == y) + " " + x + " == " + y);
-document.write("<br>");
-document.write("No es igual: " + (x != y) + " " + x + " != " + y);
-document.write("<br>");
-document.write("Estrictamente igual: " + (x === y) + " " + x + " === " + y);
-document.write("<br>");
-document.write("Desigualdad estricta: " + (x !== y) + " " + x + " !== " + y);
-document.write("<br>");
-document.write("Mayor que: " + (x > y) + " " + x + " > " + y);
-document.write("<br>");
-document.write("Mayor o igual que: " + (x >= y) + " " + x + " >= " + y);
-document.write("<br>");
-document.write("Menor que: " + (x < y) + " " + x + " < " + y);
-document.write("<br>");
-document.write("Menor o igual que: " + (x <= y) + " " + x + " <= " + y);
-document.write("<br>");
+
+document.write(`x: ${x} <br>`);
+document.write(`y: ${y} <br>`);
+document.write(`Igual: ${x == y} ${x} == ${y} <br>`);
+document.write(`No es igual: ${x != y} ${x} != ${y} <br>`);
+document.write(`Estrictamente igual: ${x === y} ${x} === ${y} <br>`);
+document.write(`Desigualdad estricta: ${x !== y} ${x} !== ${y} <br>`);
+document.write(`Mayor que: ${x > y} ${x} > ${y} <br>`);
+document.write(`Mayor o igual que: ${x >= y} ${x} > ${y} <br>`);
+document.write(`Menor que: ${x < y} ${x} > ${y} <br>`);
+document.write(`Menor o igual que: ${x <= y} ${x} > ${y} <br>`);
